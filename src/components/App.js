f26c6561a67c0decb799dd0cc0ef19212b506c92
Maryna Skrypnyk <marina.skrypnyk@gmail.com>
2022-02-03 19:37:23 +0200

    change

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import GoogleMap from './GoogleMaps/GoogleMaps';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Google Maps App</h1>
+      <GoogleMap />
+    </div>
+  );
+}
+
+export default App;
